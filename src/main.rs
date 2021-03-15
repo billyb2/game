c7d044d9a3fa6101ffaa971ff2993f011f96f41a
William Batista <bootlegbilly@protonmail.ch>
2021-03-14 21:55:44 -0400

    Added input
    There's now keyboard input for movement

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,34 @@
-use ggez;
-use ggez::{event, graphics, input, nalgebra as na};
+use ggez::{event, graphics, nalgebra as na};
+use ggez::conf::WindowSetup;
+use ggez::input::keyboard::{is_key_pressed, KeyCode};
+use ggez::input::mouse;
 
 struct MainState {
     x: f32,
-    going_right: bool,
+    y: f32,
 }
 
 impl MainState {
     fn new() -> ggez::GameResult<MainState> {
-        let s = MainState { x: 0.0, going_right: true};
+        let s = MainState { x: 0.0, y: 0.0};
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
-        if self.x < 500.0 && self.going_right {
-            self.x += 1.0;
-            
-        } else if self.x == 500.0 && self.going_right {
-            self.going_right = false;
-            
-        } else if self.x > 0.0 && !self.going_right {
-            self.x -= 1.0
-            
-        } else if self.x == 0.0 && !self.going_right {
-            self.going_right = true;
-            
+        if is_key_pressed(ctx, KeyCode::D) || is_key_pressed(ctx, KeyCode::Right) {
+            self.x += 10.0;
+        } if is_key_pressed(ctx, KeyCode::A) || is_key_pressed(ctx, KeyCode::Left) {
+            self.x -= 10.0;
+        } if is_key_pressed(ctx, KeyCode::W) || is_key_pressed(ctx, KeyCode::Up) {
+            self.y -= 10.0;
+        } if is_key_pressed(ctx, KeyCode::S) || is_key_pressed(ctx, KeyCode::Down) {
+            self.y += 10.0;
         }
         
-        if input::mouse::button_pressed(&ctx, input::mouse::MouseButton::Left) {
-            println!("Hello mouse!");
+        if mouse::button_pressed(&ctx, mouse::MouseButton::Left) {
+            //println!("Hello mouse!");
             
         }
         
@@ -40,7 +38,7 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
         
-        let rect = graphics::Rect::new(self.x, 100.0, 100.0, 100.0);
+        let rect = graphics::Rect::new(self.x, self.y, 25.0, 25.0);
 
         let rect_to_draw = graphics::Mesh::new_rectangle(
             ctx,
@@ -58,6 +56,7 @@ impl event::EventHandler for MainState {
 
 pub fn main() -> ggez::GameResult { 
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let cb = cb.window_setup(WindowSetup::default());
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut MainState::new()?;
     event::run(ctx, event_loop, state)
