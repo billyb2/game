97c381f46b3831d8b68cdcc0647b52fe81d3f406
William Batista <bootlegbilly@protonmail.ch>
2021-04-03 16:49:36 -0400

    Maps now save multiple layers
    So the map was rendering weirdly and I was fairly sure it was an issue with the .js script, and it turns out that I was right. I wasn't allocating enough space in the u8 array for multiple layers, so I'm instead just having a regular array that I turn into a byte array.

--- tiled/custom.js
+++ tiled/custom.js
@@ -3,26 +3,18 @@ var customMapFormat = {
     extension: "custom",
 
     write: function(map, fileName) {
-        let byte_array = new Uint8Array((map.width * map.height) * 22 + 8);
+        let array = [];;
 
-        byte_array[0] = (map.width * 15 & 0xff000000) >> 24;
-        byte_array[1] = (map.width * 15 & 0x00ff0000) >> 16;
-        byte_array[2] = (map.width * 15 & 0x0000ff00) >> 8;
-        byte_array[3] = (map.width * 15 & 0x000000ff);
+        array.push((map.width * 15 & 0xff000000) >> 24);
+        array.push((map.width * 15 & 0x00ff0000) >> 16);
+        array.push((map.width * 15 & 0x0000ff00) >> 8);
+        array.push((map.width * 15 & 0x000000ff));
 
-        byte_array[4] = (map.height * 15 & 0xff000000) >> 24;
-        byte_array[5] = (map.height * 15 & 0x00ff0000) >> 16;
-        byte_array[6] = (map.height * 15 & 0x0000ff00) >> 8;
-        byte_array[7] = (map.height * 15 & 0x000000ff);
+        array.push((map.height * 15 & 0xff000000) >> 24);
+        array.push((map.height * 15 & 0x00ff0000) >> 16);
+        array.push((map.height * 15 & 0x0000ff00) >> 8);
+        array.push((map.height * 15 & 0x000000ff));
 
-        for (var i = 0; i <= 7; i++) {
-            console.log(byte_array[i]);
-        }
-
-        let index = 8;
-
-
-        var rows = [];
         for (i = map.layerCount  - 1; i >= 0; i--) {
             var layer = map.layerAt(i);
 
@@ -31,77 +23,72 @@ var customMapFormat = {
                     for (x = 0; x < layer.width; ++x)
                         if (layer.tileAt(x, y) != null) {
                             // x coor
-                            byte_array[index + 0] = (x * 15 & 0xff000000) >> 24;
-                            byte_array[index + 1] = (x * 15 & 0x00ff0000) >> 16;
-                            byte_array[index + 2] = (x * 15 & 0x0000ff00) >> 8;
-                            byte_array[index + 3] = (x * 15 & 0x000000ff);
+                            array.push((x * 15 & 0xff000000) >> 24);
+                            array.push((x * 15 & 0x00ff0000) >> 16);
+                            array.push((x * 15 & 0x0000ff00) >> 8);
+                            array.push((x * 15 & 0x000000ff));
 
                             //y coor
-                            byte_array[index + 4] = (y * 15 & 0xff000000) >> 24;
-                            byte_array[index + 5] = (y * 15 & 0x00ff0000) >> 16;
-                            byte_array[index + 6] = (y * 15 & 0x0000ff00) >> 8;
-                            byte_array[index + 7] = (y * 15 & 0x000000ff);
+                            array.push((y * 15 & 0xff000000) >> 24);
+                            array.push((y * 15 & 0x00ff0000) >> 16);
+                            array.push((y * 15 & 0x0000ff00) >> 8);
+                            array.push(y * 15 & 0x000000ff);
 
                             //width
-                            byte_array[index + 8] = (15 & 0xff000000) >> 24;
-                            byte_array[index + 9] = (15 & 0x00ff0000) >> 16;
-                            byte_array[index + 10] = (15 & 0x0000ff00) >> 8;
-                            byte_array[index + 11] = (15 & 0x000000ff);
+                            array.push((15 & 0xff000000) >> 24);
+                            array.push((15 & 0x00ff0000) >> 16);
+                            array.push((15 & 0x0000ff00) >> 8);
+                            array.push(15 & 0x000000ff);
 
                             //height
-                            byte_array[index + 12] = (15 & 0xff000000) >> 24;
-                            byte_array[index + 13] = (15 & 0x00ff0000) >> 16;
-                            byte_array[index + 14] = (15 & 0x0000ff00) >> 8;
-                            byte_array[index + 15] = (15 & 0x000000ff);
+                            array.push((15 & 0xff000000) >> 24);
+                            array.push((15 & 0x00ff0000) >> 16);
+                            array.push((15 & 0x0000ff00) >> 8);
+                           array.push(15 & 0x000000ff);
 
                             if (layer.tileAt(x, y).property("player_spawn") == true) {
-                                byte_array[index + 16] = 255;
+                                array.push(255);
 
                             } else {
-                                byte_array[index + 16] = 0;
+                                array.push(0);
 
                             }
 
                             if (layer.tileAt(x, y).property("player_collidable") == true) {
-                                byte_array[index + 17] = 255;
+                                array.push(255);
 
                             } else {
-                                byte_array[index + 17] = 0;
+                                array.push(0);
 
                             }
 
-                            byte_array[index + 18] = layer.tileAt(x, y).property("red");
-                            byte_array[index + 19] = layer.tileAt(x, y).property("green");
-                            byte_array[index + 20] = layer.tileAt(x, y).property("blue");
-                            byte_array[index + 21] = layer.tileAt(x, y).property("alpha");
-                            byte_array[index + 22] = 0;
+                            array.push(layer.tileAt(x, y).property("red"));
+                            array.push(layer.tileAt(x, y).property("green"));
+                            array.push(layer.tileAt(x, y).property("blue"));
+                            array.push(layer.tileAt(x, y).property("alpha"));
+                            array.push(0);
 
-                            for (var j = index; j < index + 22; j ++) {
-                                if (byte_array[j] != 0) {
+
+                            for (var z = 0; z < array.length; z += 23) {
+                                var is_null = true;
+
+                                for (var j = 0; j <= 22; j ++) {
+                                    if (array[j + z] != 0) {
+                                        is_null = false;
+                                        break;
+
+                                    }
+                                }
+
+                                if (is_null == true) {
                                     index += 23;
                                     break;
 
                                 }
-
                             }
 
-                            rows.push({
-                                data: {
-                                    x: x * 15,
-                                    y: y * 15,
-                                    w: 15,
-                                    h: 15
-                                },
-                                player_spawn: layer.tileAt(x, y).property("player_spawn"),
-                                player_collidable: layer.tileAt(x, y).property("player_collidable"),
-                                color: [layer.tileAt(x, y).property("red") / 255.0,
-                                            layer.tileAt(x, y).property("green") / 255.0,
-                                            layer.tileAt(x, y).property("blue") / 255.0,
-                                            layer.tileAt(x, y).property("alpha") / 255.0]
-                                //health: 100
 
 
-                            });
 
                         }
 
@@ -110,7 +97,10 @@ var customMapFormat = {
             }
         }
 
+
         var file = new BinaryFile(fileName, BinaryFile.WriteOnly);
+
+        let byte_array = new Uint8Array(array);
 
         file.write(byte_array.buffer);
         file.commit();
